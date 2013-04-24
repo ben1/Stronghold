79e3131206ae572f91e766e2b23eb99f022a9229
ben <ben135@gmail.com>
2013-04-24 21:47:40 +1000

    wip

--- src/app/main.py
+++ src/app/main.py
@@ -4,5 +4,21 @@ Created on 13/04/2013
 @author: Ben
 '''
 
+import pyglet
+import pyglet.app
+
+
+class StrongholdWindow(pyglet.window.Window):
+    def __init__(self):
+        super(StrongholdWindow, self).__init__(1024, 768)
+
+        self.label = pyglet.text.Label('Hello, world!')
+
+    def on_draw(self):
+        self.clear()
+        self.label.draw()
+
 if __name__ == '__main__':
-    pass
+    window = StrongholdWindow()
+    pyglet.app.run()
+    
